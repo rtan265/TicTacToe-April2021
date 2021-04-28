37755245eba1cc34b54c04692f118c1fdadcdddd
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-04-29 00:01:59 +1200

    Added Board class, and implemented PrintBoard function

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        public char[][] boardSpaces;
+
+        public Board()
+        {
+            boardSpaces = new char[][] { new char[] { '.', '.', '.' }, new char[] { '.', '.', '.' }, new char[] { '.', '.', '.' } };
+        }
+
+        public void PlaceAPiece(char piece, int x, int y)
+        {
+            throw NotImplementedException;
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < boardSpaces.Length; i++)
+            {
+                for (int j = 0; j < boardSpaces[i].Length; j++)
+                {
+                    Console.Write(boardSpaces[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool CheckSpaceEmpty(int x, int y)
+        {
+            throw NotImplementedException;
+        }
+
+        public int CheckStatus()
+        {
+            throw NotImplementedException;
+        }
+    }
+}
+
+
+// PlaceAPiece
+// PrintBoard
+// CheckStatus
+// CheckSpaceEmpty
+// 
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,16 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            bool isGameEnded = false;
+            int currentPlayer = 1;
+            char currentPiece = 'X';
+
             Console.WriteLine("Welcome to Tic Tac Toe!");
+            Board _board = new Board();
+
+            Console.WriteLine("Here's the current board:");
+            _board.PrintBoard();
+
         }
     }
 }
