0df2b0e0c9ce3cda38ade71604fd7716d2c6a748
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-05-04 23:44:01 +1200

    Added player class

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Player
+    {
+        public bool isTurn { get; set; }
+        public char Piece { get; set; }
+
+    }
+}
