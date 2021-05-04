36438135f6ee30148881f5d5083e416718b6ba41
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-05-05 01:03:38 +1200

    Refactored Program class file

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,55 +6,8 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to Tic Tac Toe! \n");
-            Board _board = new Board();
-
-            Console.WriteLine("Here's the current board: \n");
-            _board.PrintBoard();
-
-            int x, y;
-
-            do
-            {
-                if (_board.isPlayerOne)
-                {
-                    Console.Write("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ");
-                }
-                else
-                {
-                    Console.Write("Player 2 enter a coord x,y to place your X or enter 'q' to give up: ");
-                }
-
-                string UserInput = Console.ReadLine();
-                Console.WriteLine();
-                if (UserInput == "q")
-                {
-                    if (_board.isPlayerOne)
-                    {
-                        Console.WriteLine("Player 2 has won the game!");
-                    } 
-                    else
-                    {
-                        Console.WriteLine("Player 1 has won the game!");
-                    }
-                    break;
-                }
-
-                try
-                {
-                    string[] coordinates = UserInput.Split(',');
-                    x = Int32.Parse(coordinates[0]) - 1;
-                    y = Int32.Parse(coordinates[1]) - 1;
-                    _board.PlaceAPiece(x, y);
-                    Console.WriteLine(_board.result);
-                }
-                catch
-                {
-                    Console.WriteLine("Input error, please write in the following format: x,y or 'q'  \n");
-                    _board.PrintBoard();
-                }
-            } while (!_board.isGameEnded);
+            Game newGame = new Game();
+            newGame.StartGame();
         }
     }
 }
