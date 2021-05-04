d3089c2e06cd250e62b023378b3088a2d9eb6326
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-05-04 23:43:07 +1200

    Added game class

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public Board board { get; set; }
+        public Player playerOne { get; set; }
+        public Player playerTwo { get; set; }
+        public bool isGameEnded { get; set; }
+
+
+    }
+}
